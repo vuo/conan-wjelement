dd487e7131e21e786430258e47fee9760188813f
smokris <smokris@softpixel.com>
2018-04-02 12:43:28 -0400

    Test LC_RPATH.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,16 +9,16 @@ class WJElementTestConan(ConanFile):
 
     def imports(self):
         self.copy('*', src='bin', dst='bin')
-        self.copy('*', dst='lib', src='lib')
+        self.copy('*', src='lib', dst='lib')
 
     def test(self):
-        self.run('qbs run')
+        self.run('qbs run -f "%s"' % self.source_folder)
 
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
-            self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
-            self.run('! (otool -l lib/*.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
+            self.run('! (otool -L lib/libwjelement.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -l lib/libwjelement.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libwjelement.so | grep -v "^lib/" | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
