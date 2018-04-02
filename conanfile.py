3a469b2851d1d4c0cc96ecc74eb84e8c3baf600a
smokris <smokris@softpixel.com>
2018-04-02 12:41:04 -0400

    Update LLVM package.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class WJElementConan(ConanFile):
     package_version = '3'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'llvm/3.3-1@vuo/stable'
+    requires = 'llvm/3.3-2@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/netmail-open/wjelement'
     license = 'https://github.com/netmail-open/wjelement/blob/master/COPYING'
